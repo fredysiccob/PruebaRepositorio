2bd587c3dbc0bce5a4aae19ff9422fc2bc2d1ac2
fredy palma <fpalma@siccob.com.mx>
2021-06-21 11:56:51 -0500

    Limpiando comentarios

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import {render} from "react-dom";
 import App from './components/App';
 import './css/styles.css';
 
+//Este es un comentario del index
 render(<App />, document.getElementById('app'));
