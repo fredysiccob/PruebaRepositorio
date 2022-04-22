40fcf6ca37b7df09265343fa1d69be28559e4ab8
fredy palma <fpalma@siccob.com.mx>
2022-04-22 10:15:44 -0500

    Primer commit de devolpment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {render} from "react-dom";
 import App from './components/App';
 import './css/styles.css';
 
-
 //Este es un comentario del index
 render(<App />, document.getElementById('app'));
+
+//Comentario de commit 1
