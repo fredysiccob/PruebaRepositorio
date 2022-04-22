b9f206056252bbd587df0cc90597a952f615b905
fredysiccob <45602896+fredysiccob@users.noreply.github.com>
2022-04-22 10:55:08 -0500

    Version 1.2.2
    Comentarios de devolpment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import {render} from "react-dom";
 import App from './components/App';
 import './css/styles.css';
 
-
 //Este es un comentario del index
 render(<App />, document.getElementById('app'));
+
+//Comentario de commit 1
+//Comentario de commit 2
+// Comentario de commit 3
+// Comentario de commit 4
+// Comentario de commit 5
